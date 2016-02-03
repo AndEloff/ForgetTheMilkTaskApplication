08923b0825426c687d9b8dfd96d9be587e3ed4f6
Eloff <andeloff@gmail.com>
2016-02-03 15:15:25 -0600

    Parsing out the Due Date
    Now when you add a task, you can add a due date to it. Its not 100
    percent yet, right now the Regex is not fully set up so currently for
    the due date it will grab the current year, Ive hardcoded the month to
    be May, but the day is the only one set up correctly right now. Next
    Steps: To add on to the regex to grab whatever year the user inputs as
    well as any proper month they put in. The more I watch these videos the
    more I learn and the more I want to create on my own!!

--- ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions; //this is for using the Regex
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,13 +15,31 @@ namespace ForgetTheMilk.Controllers
         }
 
         //setting up a field called tasks that stores a list of strings via the textbox
-        public static readonly List<string> Tasks = new List<string>();
+        public static readonly List<Task> Tasks = new List<Task>();
 
         [HttpPost]
         public ActionResult Add(string task)
         {
-            Tasks.Add(task);
+            var taskItem = new Task { Description = task };
+            //parsing out due date if user submits one via Regular Expression rule that I set up
+            var dueDatePattern = new Regex(@"may\s(\d)");
+            //Creating a boolean varaible to determine if the task has an due date associated with it
+            var hasDueDate = dueDatePattern.IsMatch(task);
+            if (hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+                taskItem.DueDate = new DateTime(DateTime.Today.Year, 5, day); //hardcoded the month to May will update it to parse out the curent month
+            }
+            //new instance of the task class created below, with the descrpt set to whatever the user entered task is
+            Tasks.Add(taskItem);
             return RedirectToAction("Index");
         }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
     }
 }
