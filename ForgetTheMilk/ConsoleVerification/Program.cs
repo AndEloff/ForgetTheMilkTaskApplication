13a7e8d4ba8b5bf5627cb911385dc6dad651ce65
Eloff <andeloff@gmail.com>
2016-02-17 18:37:52 -0600

    Cleaned up the Scenarios
    Cleaned up the scenarios to make them more modular: I put each scenario
    into their own method they are not dependant of eachother, also made a
    simple success and failure method that can be reused for each scenario I
    create! Still have much to learn!

--- ForgetTheMilk/ConsoleVerification/Program.cs
+++ ForgetTheMilk/ConsoleVerification/Program.cs
@@ -10,15 +10,53 @@ namespace ConsoleVerification
     {
         static void Main(string[] args)
         {
-            while (true)
+            TestDescriptionAndNoDueDate();
+            TestMayDueDate();
+
+            Console.ReadLine();
+        }
+
+        private static void TestDescriptionAndNoDueDate()
+        {
+            var input = "Pick up groceries";
+            Console.WriteLine("Scenario: " + input);
+
+            var task = new Task(input);
+
+            var descriptionShouldBe = input;
+            DateTime? dueDateShouldBe = null;
+            var success = descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate;
+            var failureMessage = "Description: " + task.Description + "should be" + descriptionShouldBe
+                + Environment.NewLine
+                + "Due Date: " + task.DueDate + "should be" + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
+        }
+
+        private static void PrintOutcome(bool success, string failureMessage)
+        {
+            if (success)
             {
-                Console.WriteLine("Add a Task: ");
-                var input = Console.ReadLine();
-                var task = new Task(input);
-                Console.WriteLine("Description: " + task.Description);
-                Console.WriteLine("Due Date: " + task.DueDate);
-                Console.WriteLine();
+                Console.WriteLine("SUCCESS");
             }
+            else
+            {
+                Console.WriteLine("ERROR: ");
+                Console.WriteLine(failureMessage);
+            }
+            Console.WriteLine();
+        }
+
+        private static void TestMayDueDate() 
+        {
+            var input = "Pick up groceries may 5";
+            Console.WriteLine("Scenario: " + input);
+
+            var task = new Task(input);
+
+            var dueDateShouldBe = new DateTime(DateTime.Today.Year, 5, 5);
+            var success = dueDateShouldBe == task.DueDate;
+            var failureMessage = "Due Date: " + task.DueDate + "should be " + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
         }
     }
 }
