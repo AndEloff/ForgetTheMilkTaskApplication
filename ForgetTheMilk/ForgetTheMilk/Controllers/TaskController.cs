0bb0071a4ae160cb57d5f5c3539c140d5002bee7
Eloff <andeloff@gmail.com>
2016-02-22 15:40:13 -0600

    Added Monthly NUnit Tests
    Created a test cases that covers all day in each of the months. Regex
    needs to be researched to be refined better than what it is now.

--- ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions; //this is for using the Regex
 using System.Web;
@@ -33,14 +34,16 @@ namespace ForgetTheMilk.Controllers
         {
             Description = task;
             //parsing out due date if user submits one via Regular Expression rule that I set up
-            var dueDatePattern = new Regex(@"may\s(\d)");
+            var dueDatePattern = new Regex(@"(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\s(\d)");
             //Creating a boolean varaible to determine if the task has an due date associated with it
             var hasDueDate = dueDatePattern.IsMatch(task);
             if (hasDueDate)
             {
                 var dueDate = dueDatePattern.Match(task);
-                var day = Convert.ToInt32(dueDate.Groups[1].Value);
-                DueDate = new DateTime(today.Year, 5, day);
+                var monthInput = dueDate.Groups[1].Value;
+                var month = DateTime.ParseExact(monthInput, "MMM",CultureInfo.CurrentCulture).Month;
+                var day = Convert.ToInt32(dueDate.Groups[2].Value);
+                DueDate = new DateTime(today.Year, month, day);
                 if (DueDate < today)
                 {
                     DueDate = DueDate.Value.AddYears(1);
